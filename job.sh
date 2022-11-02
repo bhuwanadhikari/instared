aac264451619fe57118484f0daf47daede0d8ab6
Bhuwan Adhikari <bhuwan.adhikari@lastingdynamics.com>
2022-11-03 00:23:28 +0545

    update command

--- job.sh
+++ job.sh
@@ -1,2 +1,2 @@
 echo "Time of execution -----------------> `date "+%Y-%m-%d %H:%M:%S"`";
-~/.nvm/versions/node/v16.14.0/bin/node ~/Documents/learns/instareddit/build/index
+export NODE_ENV=production && ~/.nvm/versions/node/v16.14.0/bin/node ~/Documents/learns/instareddit/build/index
